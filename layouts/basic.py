4724ab11dcb82c18c2c29349af263657a1cc75d2
gabrielvpina <gvpina.rodrigues@gmail.com>
2025-07-17 23:44:36 -0300

    basic code

--- layouts/basic.py
+++ layouts/basic.py
@@ -1,1 +1,14 @@
-from dash import Dash, html
+from dash import Dash, html, dash_table
+import pandas as pd
+
+df  = pd.read_csv('data/world-data-2023.csv')
+
+app = Dash()
+
+app.layout = [
+    html.H1("World Data 2023"),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=15)
+]
+
+if __name__ == '__main__':
+    app.run(debug=True)
